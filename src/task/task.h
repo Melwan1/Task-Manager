7c3639cd2b4da51ae52a27bc7893c884d9cfc332
Melwan Chevassus <melwan.chevassus@epita.fr>
2023-12-19 12:07:24 +0100

    Task-Manager: task.h: finish task_create function documentation

--- src/task/task.h
+++ src/task/task.h
@@ -16,8 +16,22 @@ struct task
     struct timestamp due_date;
     struct timestamp creation_date;
     char *tag;
-    char *description;
+    char *title;
     char *comment;
 };
+
+/* task_create
+** Creates a task and returns it.
+** params: 
+**
+** - due_date: string, represents the due_date. The format of the string is "YYYY-MM-DD HH:MM:SS".
+** - tag: string, the tag of the task. Looks like "CPXA", "STA", "NET1", ...
+** - title: string, the title of the task, essentially what to do.
+** - comment: string or NULL, an optional comment of the task.
+** 
+** return: a pointer to the created task.
+*/ 
+
+struct task *task_create(char *due_date, char *tag, char *title, char *comment);
 
 #endif /* !TASK_H */
