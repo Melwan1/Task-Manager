11a9983eeedbae6bf7501bf48deb5612c45f1d65
Melwan Chevassus <melwan.chevassus@epita.fr>
2023-12-19 13:10:23 +0100

    Task-Manager: move timestamp from task.h to date.h

--- src/task/task.h
+++ src/task/task.h
@@ -1,16 +1,6 @@
 #ifndef TASK_H
 #define TASK_H
 
-struct timestamp
-{
-    int year;
-    int month;
-    int day;
-    int hour;
-    int minute;
-    int second;
-};
-
 struct task 
 {
     struct timestamp *due_date;
--- src/utils/date/date.h
+++ src/utils/date/date.h
@@ -1,4 +1,14 @@
 #ifndef DATE_H
 #define DATE_H
 
+struct timestamp
+{
+    int year;
+    int month;
+    int day;
+    int hour;
+    int minute;
+    int second;
+};
+
 #endif /* !DATE_H */
