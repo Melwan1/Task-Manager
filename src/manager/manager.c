f3e7b6c9c825c69cdf9c48bd6ceebd6f2f66b528
Melwan Chevassus <melwan.chevassus@epita.fr>
2023-12-25 14:46:16 +0100

    Task-Manager: manager.c: add create_category function

---
+++ src/manager/manager.c
@@ -0,0 +1,33 @@
+#include "manager.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+#include "../task/task.h"
+
+struct task_category *create_category(char *name)
+{
+    if (!name)
+    {
+        fprintf("Task-Manager: create_category: could not create, name is NULL\n");
+        return NULL;
+    }
+    struct task_category *new_category = malloc(sizeof(*new_category));
+    if (!new_category)
+    {
+        fprintf("Task-Manager: create_category: malloc failed\n");
+        return NULL;
+    }
+    new_category->name = name;
+    new_category->task_count = 0;
+    new_category->tasks = malloc(sizeof(struct task));
+    if (!new_category->tasks)
+    {
+        free(new_category);
+        fprintf("Task-Manager: create_category: malloc failed\n");
+        return NULL;
+    }
+    new_category->tasks[0] = NULL;
+    return new_category;
+}
